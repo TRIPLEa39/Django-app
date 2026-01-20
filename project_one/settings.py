5ee7cc3e1d9085781c2b933e316d077db72b76db
TRIPLEa39 <adamelmhlawy22@gmail.com>
2026-01-20 15:17:35 +0200

    Move email/password to env

--- project_one/settings.py
+++ project_one/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adamelmhlawy22@gmail.com'
-EMAIL_HOST_PASSWORD = 'xowf gscv zcwr eizp '
+EMAIL_HOST_USER =  os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =  os.getenv("EMAIL_HOST_PASSWORD")
